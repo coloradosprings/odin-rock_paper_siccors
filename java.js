9db806d31c3022e088592156b87ba4269f328791
Josef Wagner <josef.wagner0707@gmail.com>
2022-08-04 15:36:04 +0200

    create ui for rock-paper-sciccors

--- java.js
+++ java.js
@@ -1,18 +1,85 @@
-console.log(Math.floor(1.8))
 
+const buttons = document.querySelectorAll('button')
+const score = document.createElement('div')
+const div = document.createElement('div') 
+const para = document.createElement('p') 
+
+
+score.classList.add('score')
+para.classList.add('score')
+div.classList.add('score')
+
+function playSelection(e){
+    selection = e.path[1].classList[0]
+    result = playgame(selection) 
+    Game(result)  
+}
+
+let playerScore = 0
+let computerScore = 0
+score.textContent = '0 : 0'
+let round = 0
+
+document.body.appendChild(score)
+function Game(result){
+    
+    round += 1
+    console.log(round)
+    if(result == r2){
+        playerScore += 1
+    }
+    else if(result == r3){
+        computerScore += 1
+    }
+    else{}
+    
+
+    score.textContent = `${playerScore} : ${computerScore}`;
+    para.textContent = `${result}`;
+    score.appendChild(para);
+    round >= 5 ? reset(playerScore,computerScore) : NaN
+
+}
+function imageScale(e){(
+    console.log(e)
+)}
+buttons.forEach((button) => {
+    button.addEventListener('click',playSelection)
+    button.addEventListener("mouseover",imageScale)
+})
+
+
+let reset = function(player,computer){
+    round = 0 
+    score.textContent = ''
+    para.textContent = ''
+    score.appendChild(div)
+    playerScore = 0
+    computerScore = 0
+    if(computer > player){
+        div.textContent = `computer wins! Final score: ${player} : ${computer}`        
+    }
+    else if(computer < player){
+        div.textContent = `player wins! Final score: ${player} : ${computer}`        
+    }
+    else{
+        div.textContent = `it's a draw! Final score: ${player} : ${computer}`
+    }
+  
+
+}
 
 let getComputerChoice = () => {
     numb = Math.floor(Math.random()*3+1)
-    computerchoice = numb == 1 ? "rock" : numb == 2 ? "siccors": "paper"
+    computerchoice = numb == 1 ? "rock" : numb == 2 ? "sciccors": "paper"
     return computerchoice
 } 
 
-function playgame(s2){
-    s1 = prompt("Rock Paper Siccors")
+function playgame(s1,s2 = getComputerChoice()){
     playerSelection = s1.toLowerCase()
     computerSelection = s2.toLowerCase()
     a = "rock"
-    b = "siccors"
+    b = "sciccors"
     c = "paper"
     console.log(computerSelection,playerSelection)
     r1 = `It's even! ${playerSelection} and ${computerSelection}`
@@ -33,24 +100,7 @@ function playgame(s2){
     }
 }
 
-function play(rounds = 5){
-    playerScore = 0
-    computerscore = 0
-    for (let i = 0; i < rounds;i++) {
-        console.log(i)
-        r = playgame(getComputerChoice())
-        r == r1 ? playerScore++ : r == r2 ? computerscore++ : 0
-        if(i == 4)
-        {if(playerScore > computerscore){
-            return `player wins ! ${playerScore} : ${computerscore}`
-        }
-        else if(playerScore == computerscore){
-            return `draw! ${playerScore} : ${computerscore}`
-        }
-        else{
-            return `computer wins! ${computerscore} : ${playerScore}`
-        }}
 
-    }
-}
-console.log(play())
+
+    
+
